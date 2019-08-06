143d9dc200b64c852d71788c25fe3704b822a0fe
Andrei Gavra <andrei.gavra89@gmail.com>
2019-08-06 23:34:26 +0300

    Fix enemy stucking

--- Assets/Scripts/src/Base/EnemyBase.cs
+++ Assets/Scripts/src/Base/EnemyBase.cs
@@ -1,6 +1,8 @@
-﻿using UnityEngine;
-using src.Helpers;
+﻿using src.Helpers;
 using src.Managers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 public abstract class EnemyBase : MonoBehaviour, IExplosable
 {
@@ -11,6 +13,10 @@ public abstract class EnemyBase : MonoBehaviour, IExplosable
     protected Rigidbody2D Rigidbody2d { get; set; }
     protected float Speed { get; set; }
     protected Vector2 Direction { get; set; }
+
+    private List<Vector3> _allowedDirections = new List<Vector3>();
+    private bool _isStucked = false;
+    private System.Random _random = new System.Random();
 
     // Start is called before the first frame update
     protected void Start()
@@ -23,6 +29,10 @@ public abstract class EnemyBase : MonoBehaviour, IExplosable
     protected void FixedUpdate()
     {
         if (gameStateManager.IsGamePaused || gameStateManager.IsPlayerMovementForbidden) {return;}
+        if(_isStucked)
+        {
+            Unstuck();
+        }
         Rigidbody2d.MovePosition(Rigidbody2d.position + Direction * Speed * Time.deltaTime);
     }
 
@@ -42,7 +52,13 @@ public abstract class EnemyBase : MonoBehaviour, IExplosable
     public void OnCollisionEnter2D(Collision2D col)
     {
         MoveToCenterOfTheCell();
-        Direction = _directions.ChoseRandomExcept(Direction);
+        Unstuck();
+    }
+
+    public void OnCollisionStay2D(Collision2D col)
+    {
+        MoveToCenterOfTheCell();
+        Unstuck();
     }
 
     protected void MoveToCenterOfTheCell()
@@ -61,5 +77,39 @@ public abstract class EnemyBase : MonoBehaviour, IExplosable
     protected Vector2 ChooseRandomExceptCertainDirection(Vector2 direction)
     {
         return _directions.ChoseRandomExcept(direction);
+    }
+
+    private void Unstuck()
+    {
+        _allowedDirections.Clear();
+        StartCoroutine(CheckForObstacle(Vector3.down));
+        StartCoroutine(CheckForObstacle(Vector3.left));
+        StartCoroutine(CheckForObstacle(Vector3.up));
+        StartCoroutine(CheckForObstacle(Vector3.right));
+        if(_allowedDirections.Count == 0)
+        {
+            _isStucked = true;
+        }
+        else
+        {
+            var index = _random.Next(_allowedDirections.Count);
+            Direction = _allowedDirections[index];
+            _isStucked = false;
+        }
+    }
+
+    private IEnumerator CheckForObstacle(Vector3 direction)
+    {
+        var layerMask = (1 << 8) | (1 << 9);
+        var currentPosition = transform.position;
+
+        var hit = Physics2D.Raycast(new Vector2(currentPosition.x + 0.5f, currentPosition.y + 0.5f), direction, 1, layerMask);
+
+       if (!hit.collider)
+       {
+            _allowedDirections.Add(direction);
+       }
+
+        yield return new WaitForSeconds(0.05f);
     }
 }
