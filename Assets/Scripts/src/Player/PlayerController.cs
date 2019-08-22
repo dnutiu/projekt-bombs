22830ee4bec1f594eb32b092eb30b26684435499
Denis Nuțiu <denis.nutiu@gmail.com>
2019-08-22 20:39:20 +0300

    Implement player death and godmode

--- Assets/Scripts/src/Player/PlayerController.cs
+++ Assets/Scripts/src/Player/PlayerController.cs
@@ -1,51 +1,60 @@
 ﻿using src.Ammo;
 using src.Base;
 using src.Helpers;
-using src.Interfaces;
 using src.Managers;
 using UnityEngine;
-using UnityStandardAssets.CrossPlatformInput;
 
 namespace src.Player
 {
-    public class PlayerController : PlayerBase
+    public class PlayerController : GameplayComponent, IExplosable
     {
-        private GameStateManager _gameStateManager = GameStateManager.Instance;
+        private readonly GameStateManager _gameStateManager = GameStateManager.Instance;
+        private Rigidbody2D _rigidbody2d;
+        private Collider2D _collider2D;
         private Transform _respawnPosition;
         private BombsSpawner _bombsSpawner;
         private Animator _animator;
         private PlayerUpgrade _playerUpgrade;
         private static readonly int AnimHorizontal = Animator.StringToHash("AnimHorizontal");
         private static readonly int AnimVertical = Animator.StringToHash("AnimVertical");
+        private static readonly int AnimDeath = Animator.StringToHash("AnimDeath");
 
-        protected new void Start()
+
+        public bool godMode;
+        public float movementSpeed = 4f;
+        private bool _isDead;
+
+        protected void Start()
         {
-            base.Start();
-
+            _rigidbody2d = GetComponent<Rigidbody2D>();
+            _collider2D = GetComponent<Collider2D>();
+            _animator = GetComponentInChildren<Animator>();
             _respawnPosition = GameObject.Find("RespawnPosition").transform;
             _bombsSpawner = GameObject.Find("BombSpawner").GetComponent<BombsSpawner>();
-            _animator = GetComponentInChildren<Animator>();
-            _playerUpgrade = PlayerUpgrade.Instance;
-
-            movementSpeed = _playerUpgrade.GetMovementSpeed();
-            _playerUpgrade.PlayerSpeed += IncreaseSpeed;
             
+            _playerUpgrade = PlayerUpgrade.Instance;
+            movementSpeed = _playerUpgrade.GetMovementSpeed();
+            _playerUpgrade.PlayerSpeed += OnSpeedUpgrade;
+
+#if UNITY_EDITOR
+            godMode = true;
+#endif
             Respawn();
         }
 
         private void FixedUpdate()
         {
-            if (_gameStateManager.IsGamePaused || _gameStateManager.IsPlayerMovementForbidden) {return;}
-            HandleMovement();
+            if (_gameStateManager.IsGamePaused || _gameStateManager.IsPlayerMovementForbidden || _isDead) {return;}
+            HandleMovementInput();
         }
 
         private void Update()
         {
-            if (_gameStateManager.IsGamePaused || _gameStateManager.IsPlayerMovementForbidden) {return;}
-            HandleBomb();
+            if (_gameStateManager.IsGamePaused || _gameStateManager.IsPlayerMovementForbidden || _isDead) {return;}
+            HandleBombInput();
         }
 
-        private void HandleMovement()
+        private void HandleMovementInput()
         {
 #if UNITY_STANDALONE || UNITY_WEBGL || UNITY_EDITOR
             var horizontal = Input.GetAxisRaw("Horizontal");
@@ -79,7 +88,7 @@ namespace src.Player
             _animator.SetFloat(AnimVertical, movementVector.y);
 
 
-            rigidbody2d.MovePosition(rigidbody2d.position + movementSpeed * Time.deltaTime * movementVector);
+            _rigidbody2d.MovePosition(_rigidbody2d.position + movementSpeed * Time.deltaTime * movementVector);
         }
 
         private void PlaceBomb()
@@ -87,7 +96,7 @@ namespace src.Player
             _bombsSpawner.PlaceBomb(transform);
         }
 
-        private void HandleBomb()
+        private void HandleBombInput()
         {
 #if UNITY_EDITOR || UNITY_STANDALONE || UNITY_WEBGL
             if (Input.GetKeyDown(KeyCode.Space))
@@ -117,6 +126,47 @@ namespace src.Player
             {
                 other.isTrigger = false;
             }
+        }
+        
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.CompareTag("Explosion"))
+            {
+                OnExplosion();
+            }
+            if (other.CompareTag("Enemy"))
+            {
+                OnContactWithEnemy();
+            }
+        }
+
+        private void Die()
+        {
+            if (godMode)
+            {
+                return;
+            }
+            _isDead = true;
+            _collider2D.enabled = false;
+            _animator.SetBool(AnimDeath, true);
+            Destroy(gameObject, 0.7f);
+        }
+
+        public void OnExplosion()
+        {
+            DebugHelper.LogInfo("Player hit by explosion");
+            Die();
+        }
+
+        private void OnContactWithEnemy()
+        {
+            DebugHelper.LogInfo("Player hit by enemy");
+            Die();
+        }
+
+        private void OnSpeedUpgrade(float speed)
+        {
+            movementSpeed += speed;
         }
     }
 }
